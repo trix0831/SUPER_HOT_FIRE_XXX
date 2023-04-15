8cf6b3d99844acf941ecc71b1b6b82e51284317a
trix0831 <joseph831hsu@gmail.com>
2023-04-15 16:16:25 +0800

    Create repos.

---
+++ SUPER_HOT_FIRE_XXX.ino
@@ -0,0 +1,110 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include "RFID.h"
+#include <string.h>
+#include "straight_turn.h"
+
+#define RST_PIN 9
+#define SS_PIN 53
+
+// MFRC522 *mfrc522;
+
+#define PWMA 11
+#define AIN2 3
+#define AIN1 2
+#define BIN1 5
+#define BIN2 6
+#define PWMB 12
+
+#define speed 128
+
+char val;
+char val2;
+byte testid;
+
+void motorWriting(int, int);
+
+void setup() {
+
+  // put your setup code here, to run once:
+  Serial.begin (9600);
+  Serial.println("serial ready");
+  Serial1.begin(9600);
+  Serial1.println("BT ready");
+  pinMode(13, OUTPUT);
+
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+
+  // RFID
+  SPI.begin();
+  mfrc522 = new MFRC522(SS_PIN, RST_PIN);
+  // 請系統去要一塊記憶體空間，後面呼叫它的建構函式
+  // 將(SS, RST) 當成參數傳進去初始化。
+  mfrc522->PCD_Init();
+  /* 初始化MFRC522讀卡機 PCD_Init 模組。 -> 表示：
+  透過記憶體位置，找到 mfrc522 這物件，再翻其內容。*/
+  Serial.println(F("Read UID on a MIFARE PICC:"));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  analogWrite(PWMA, speed);
+  analogWrite(PWMB, speed);
+
+  if (Serial.available()) {
+    val = Serial.read();
+    Serial1.println(val);
+  }
+
+
+  if (Serial1.available()) {
+    val2 = Serial1.read();
+    Serial.println(val2);
+
+
+    if(val2 == 'w'){
+      motorWriting(speed, speed);
+    }else if(val2 == 's'){
+      motorWriting(-speed, -speed);
+    }else if(val2 == 'a'){
+      motorWriting(0, speed);
+    }else if(val2 == 'd'){
+      motorWriting(speed, 0);
+    }else if(val2 == 'p'){
+      motorWriting(0,0);
+    }
+  }
+
+  rfid(testid);
+
+  straight();
+
+  if (checkState() == 11111){//1 is black, 2 is white
+    if(stage % 2 == 0){ // U turn 
+      UTurn();
+    }
+
+    if(stage % 6 == 1){ // right turn
+      RightTurn();
+    }
+
+    if(stage%6 == 3){ // middle Node -> go straight
+      while (checkState() == 11111){//1 is black, 2 is white
+        motorWriting(speed, speed);
+      }
+    }
+
+    if(stage%6 == 5){ // left turn
+      LeftTurn();
+    }
+  }
+
+}
+
+
---
+++ straight_turn.h
@@ -0,0 +1,160 @@
+#ifndef STRAIGHT_TURN_H
+#define STRAIGHT_TURN_H
+
+#define PWMA 11
+#define AIN1 2
+#define AIN2 3
+#define BIN1 5
+#define BIN2 6
+#define PWMB 12
+
+#define IR1 32
+#define IR2 34
+#define IR3 36
+#define IR4 38
+#define IR5 40
+
+#define speed 140
+
+int lastState;
+int stage = 0;
+
+void UTurn();
+void RightTurn();
+void LeftTurn();
+void straight();
+int checkState();
+void motorWriting(int, int);
+
+void UTurn(){
+  while (!(checkState() == 22221)){//1 is black, 2 is white
+        motorWriting(speed, -speed);
+    }
+}
+
+void RightTurn(){
+  motorWriting(speed, speed);
+  delay(200);
+  while (!(checkState() == 12222)){//1 is black, 2 is white
+    motorWriting(speed, 0);
+  }
+  motorWriting(speed, speed);
+  delay(300);
+}
+
+void LeftTurn(){
+  motorWriting(speed, speed);
+  delay(200);
+  while (!(checkState() == 22221)){//1 is black, 2 is white
+    motorWriting(0, speed);
+  }
+  motorWriting(speed, speed);
+  delay(300);
+}
+
+int checkState(){ //checking the states of IR, 1 means black, 2 means white
+  if (digitalRead(IR1) == HIGH && digitalRead(IR2) == LOW && digitalRead(IR3) == LOW && digitalRead(IR4) == LOW && digitalRead(IR5) == LOW){
+    lastState = 2; // not all black
+    return 12222;    
+  }
+  else if(digitalRead(IR1) == HIGH && digitalRead(IR2) == HIGH && digitalRead(IR3) == LOW && digitalRead(IR4) == LOW && digitalRead(IR5) == LOW){
+    lastState = 2; // not all black
+    return 11222;
+  }
+  else if(digitalRead(IR1) == LOW && digitalRead(IR2) == HIGH && digitalRead(IR3) == LOW && digitalRead(IR4) == LOW && digitalRead(IR5) == LOW){
+    lastState = 2; // not all black
+    return 21222;
+  }
+  else if(digitalRead(IR1) == LOW && digitalRead(IR2) == HIGH && digitalRead(IR3) == HIGH && digitalRead(IR4) == LOW && digitalRead(IR5) == LOW){
+    lastState = 2; // not all black
+    return 21122;
+  }
+  else if(digitalRead(IR1) == LOW && digitalRead(IR2) == LOW && digitalRead(IR3) == HIGH && digitalRead(IR4) == LOW && digitalRead(IR5) == LOW){
+    lastState = 2; // not all black
+    return 22122;
+  }
+  else if(digitalRead(IR1) == LOW && digitalRead(IR2) == LOW && digitalRead(IR3) == HIGH && digitalRead(IR4) == HIGH && digitalRead(IR5) == LOW){
+    lastState = 2; // not all black
+    return 22112;
+  }
+  else if(digitalRead(IR1) == LOW && digitalRead(IR2) == LOW && digitalRead(IR3) == LOW && digitalRead(IR4) == HIGH && digitalRead(IR5) == LOW){
+    lastState = 2; // not all black
+    return 22212;
+  }
+  else if(digitalRead(IR1) == LOW && digitalRead(IR2) == LOW && digitalRead(IR3) == LOW && digitalRead(IR4) == HIGH && digitalRead(IR5) == HIGH){
+    lastState = 2; // not all black
+    return 22211;
+  }
+  else if(digitalRead(IR1) == LOW && digitalRead(IR2) == LOW && digitalRead(IR3) == LOW && digitalRead(IR4) == LOW && digitalRead(IR5) == HIGH){
+    lastState = 2; // not all black
+    return 22221;
+  }
+  else if(digitalRead(IR1) == HIGH && digitalRead(IR2) == HIGH && digitalRead(IR3) == HIGH && digitalRead(IR4) == HIGH && digitalRead(IR5) == HIGH){
+    if (lastState == 2){
+      stage++;
+      lastState = 1;
+    }
+    return 11111;
+    
+  }
+  else if(digitalRead(IR1) == LOW && digitalRead(IR2) == LOW && digitalRead(IR3) == LOW && digitalRead(IR4) == LOW && digitalRead(IR5) == LOW){
+    return 22222;
+  }
+
+}
+
+void straight(){
+  if(checkState() == 12222){
+    motorWriting(-speed,speed);
+  }
+  else if(checkState() == 11222){
+    motorWriting(-speed/3,speed);
+  }
+  else if(checkState() == 21222){
+    motorWriting(0,speed);
+  }
+  else if(checkState() == 21122){
+    motorWriting(speed/2,speed);
+  }
+  else if(checkState() == 22122){
+    motorWriting(speed,speed);
+  }
+  else if(checkState() == 22112){
+    motorWriting(speed,speed/2);
+  }
+  else if(checkState() == 22212){
+    motorWriting(speed,0);
+  }
+  else if(checkState() == 22211){
+    motorWriting(speed,-speed/2);
+  }
+  else if(checkState() == 22221){
+    motorWriting(speed,-speed);
+  }
+}
+
+void motorWriting(int vL, int vR){
+  if(vL > 0){
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
+  }
+  else{
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, HIGH);
+  }
+
+  if(vR > 0){
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+  }
+  else{
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, HIGH);
+  }
+
+  analogWrite(PWMB, abs(vL));
+  analogWrite(PWMA, abs(vR));
+}
+
+
+#endif
